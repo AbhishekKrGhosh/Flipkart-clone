72bebe6cc83d7f113a5a9a67799d917969c030cd
Abhishek Kumar <abhishek.andri05@gmail.com>
2023-01-13 15:11:03 +0530

    Initialize project using Create React App

---
+++ index.js
@@ -0,0 +1,11 @@
+import React from "react";
+function Navbar(){
+    return(
+        <div className="MainComponent">
+            <div className = "leftSide">
+                <img alt = "image" src = "a.png"/>
+            </div>
+        </div>
+    );
+}
+export default Navbar;
